d109ea38ad0b5a152f0fe9c604c272c3aa6a4133
leonidas2002 <117978563+leonidas2002@users.noreply.github.com>
2025-11-09 12:22:23 +0100

    Add Cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
-import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter()
-    },
-    preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
